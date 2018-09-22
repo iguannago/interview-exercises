9f708160ce0915dfcb97aac1b339e90bff261b3f
david.crespo <david.crespo@hmcts.net>
2018-09-22 18:39:26 +0200

    Add X roman test

--- src/test/java/RomanNumberTest.java
+++ src/test/java/RomanNumberTest.java
@@ -11,7 +11,7 @@ public class RomanNumberTest {
     private RomanNumber romanNumber = new RomanNumber();
 
     @Test
-    @Parameters({"I, 1", "II, 2", "III, 3", "IV, 4", "V, 5", "VI, 6", "VII, 7", "VIII, 8", "IX, 9"})
+    @Parameters({"I, 1", "II, 2", "III, 3", "IV, 4", "V, 5", "VI, 6", "VII, 7", "VIII, 8", "IX, 9", "X, 10"})
     public void givenI_then_return1(String roman, int expected) {
         assertEquals(expected, romanNumber.parse(roman));
     }
