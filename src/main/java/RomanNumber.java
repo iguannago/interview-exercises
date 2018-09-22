54e1e1b3af1b7c474e9b103148b0012fd2a00ec7
david.crespo <david.crespo@hmcts.net>
2018-09-22 18:38:39 +0200

    add roman IX test and prod code to pass it

--- src/main/java/RomanNumber.java
+++ src/main/java/RomanNumber.java
@@ -2,12 +2,15 @@ class RomanNumber {
     int parse(String roman) {
         if ("I".equals(roman)) return 1;
         if ("V".equals(roman)) return 5;
+        if ("X".equals(roman)) return 10;
         else {
             if (roman.startsWith("II"))
                 return 1 + parse(roman.substring(1));
             if (roman.startsWith("IV"))
                 return parse(roman.substring(1)) - 1;
-            return 5 + parse(roman.substring(1));
+            if (roman.startsWith("VI"))
+                return 5 + parse(roman.substring(1));
+            return parse(roman.substring(1)) - 1;
         }
     }
 }
--- src/test/java/RomanNumberTest.java
+++ src/test/java/RomanNumberTest.java
@@ -11,7 +11,7 @@ public class RomanNumberTest {
     private RomanNumber romanNumber = new RomanNumber();
 
     @Test
-    @Parameters({"I, 1", "II, 2", "III, 3", "IV, 4", "V, 5", "VI, 6"})
+    @Parameters({"I, 1", "II, 2", "III, 3", "IV, 4", "V, 5", "VI, 6", "VII, 7", "VIII, 8", "IX, 9"})
     public void givenI_then_return1(String roman, int expected) {
         assertEquals(expected, romanNumber.parse(roman));
     }
