bd8a368e0cd1ce0194547819f6b1c9af197cad9c
david.crespo <david.crespo@hmcts.net>
2018-09-22 18:12:45 +0200

    Add logic to parse roman numbers from 1 to 5

--- src/main/java/RomanNumber.java
+++ src/main/java/RomanNumber.java
@@ -1,8 +1,11 @@
 public class RomanNumber {
     public int parse(String romanNumber) {
         if ("I".equals(romanNumber)) return 1;
+        if ("V".equals(romanNumber)) return 5;
         else {
-            return 1 + parse(romanNumber.substring(1));
+            if (romanNumber.startsWith("II"))
+                return 1 + parse(romanNumber.substring(1));
+            return parse(romanNumber.substring(1)) - 1;
         }
     }
 }
