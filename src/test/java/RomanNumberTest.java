28d6423d1bbf1280d8e352795180bbeeb2da5140
david.crespo <david.crespo@hmcts.net>
2018-09-23 11:24:26 +0200

    Add more tests to confirm the algorithm works up to 39 roman numbers

--- src/test/java/RomanNumberTest.java
+++ src/test/java/RomanNumberTest.java
@@ -12,7 +12,8 @@ public class RomanNumberTest {
 
     @Test
     @Parameters({"I, 1", "II, 2", "III, 3", "IV, 4", "V, 5", "VI, 6", "VII, 7", "VIII, 8", "IX, 9", "X, 10", "XI, 11",
-            "XII, 12", "XIII, 13"})
+            "XII, 12", "XIII, 13", "XIV, 14", "XV, 15", "XVI, 16", "XVII, 17", "XVIII, 18", "XIX, 19", "XX, 20",
+            "XXI, 21", "XXII, 22", "XXIII, 23", "XXIV, 24", "XXXIV, 34", "XXXIX, 39"})
     public void givenI_then_return1(String roman, int expected) {
         assertEquals(expected, romanNumber.parse(roman));
     }
